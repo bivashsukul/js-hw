581220bdd7baf6106b79e65758b3360592864f9e
Bivash Sukul <167380041+bivashsukul@users.noreply.github.com>
2024-08-06 00:48:08 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,62 @@
+// ex 1----
+function showText(){
+    document.getElementById('s_text').innerHTML="Hello Bangla";
+}
+
+// ex-2-------
+function showtext(){
+    document.getElementById('show_text').innerHTML="Bangladesh"
+}
+
+
+// ex-3-------
+function fontChange(){
+    document.getElementById('font_size').style.fontSize='50px'
+}
+
+
+// ex 4------
+function colorChange(){
+    document.getElementById('color_change').style.color='red'
+}
+
+// ex-5-------
+function hideText(){
+    document.getElementById('hide_text').style.display="none"
+}
+
+// ex-6-------
+function shoText(){
+    document.getElementById('sho_text').style.display="none"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
